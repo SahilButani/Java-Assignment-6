65e50527f16c4e7008898d8ff6b197d42eb02b68
Sahil Butani <sahilbutani1760@gmail.com>
2019-01-23 15:16:11 +0530

    Update List_Iterator.java

--- List_Iterator.java
+++ List_Iterator.java
@@ -58,16 +58,16 @@ public class List_Iterator {
         SList<String> sl = new SList<String>();
         System.out.println(sl);
         SListIterator<String> slIter = sl.iterator();
-
+         System.out.println("inserting \"hi\"");
         slIter.insert("hi");
         System.out.println(sl);
-
+        System.out.println("inserting \"there\"");
         slIter.insert("there");
         System.out.println(sl);
-
+        System.out.println("inserting \"sweetie\"");
         slIter.insert("sweetie");
         System.out.println(sl);
-
+        System.out.println("inserting \"pie\"");
         slIter.insert("pie");
         System.out.println(sl);
         SListIterator<String> slIter2 = sl.iterator();

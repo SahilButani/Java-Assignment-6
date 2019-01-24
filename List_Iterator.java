3c053a933732aefc477a95cc1ef848ca968f047c
Sahil Butani <sahilbutani1760@gmail.com>
2019-01-24 11:32:56 +0530

    Update List_Iterator.java

--- List_Iterator.java
+++ List_Iterator.java
@@ -1,64 +1,90 @@
 
-class SList<E> {
+class SList<E> 
+{
     private Link<E> headLink = new Link<E>(null);
-    SListIterator<E> iterator() { return new SListIterator<E>(headLink); }
-    public String toString() {
+    SListIterator<E> iterator() 
+    { 
+        return new SListIterator<E>(headLink); 
+    }
+    public String toString() 
+    {
         if(headLink.next == null) return "SList: []";
         System.out.println("SList: [");
         SListIterator<E> it = this.iterator();
         StringBuilder s = new StringBuilder();
-        while(it.hasNext()) {
+        while(it.hasNext()) 
+        {
             s.append(it.next() + (it.hasNext() ? ", " : ""));
         }
         return s + "]";
     }
 }
 
-class SListIterator<E> {
+class SListIterator<E> 
+{
     Link<E> current;
-    SListIterator(Link<E> link) {
+    SListIterator(Link<E> link) 
+    {
         current = link;
     }
-    public boolean hasNext() {
+    public boolean hasNext() 
+    {
         return current.next != null;
     }
-    public Link<E> next() {
+    public Link<E> next() 
+    {
         current = current.next;
         return current;
     }
-    public void insert(E e) {
+    public void insert(E e) 
+    {
         current.next = new Link<E>(e, current.next);
         current = current.next;
     }
-    public void remove() {
-        if(current.next != null) {
+    public void remove() 
+    {
+        if(current.next != null) 
+        {
             current.next = current.next.next;
         }
     }
 }
 
-class Link<E> {
+class Link<E> 
+{
     E e;
     Link<E> next;
-    Link(E e, Link<E> next) {
+    Link(E e, Link<E> next) 
+    {
         this.e = e;
         this.next = next;
     }
-    Link(E e) {
+    Link(E e) 
+    {
         this(e, null);
     }
-    public String toString() {
+    public String toString() 
+    {
         if(e == null) return "null";
         return e.toString();
     }
 }
 
-public class List_Iterator {
-    public static void main(String[] args) {
+public class List_Iterator 
+{
+    /*
+     *Main method creates a sList and inserts elements and then removes them.
+     */
+    public static void main(String[] args) 
+    {
+        
         SList<String> sl = new SList<String>();
+        
         System.out.println(sl);
+        
         SListIterator<String> slIter = sl.iterator();
-         System.out.println("inserting \"hi\"");
+        
+        System.out.println("inserting \"hi\"");
         slIter.insert("hi");
         System.out.println(sl);
         System.out.println("inserting \"there\"");
@@ -70,7 +96,9 @@ public class List_Iterator {
         System.out.println("inserting \"pie\"");
         slIter.insert("pie");
         System.out.println(sl);
+
         SListIterator<String> slIter2 = sl.iterator();
+        
         System.out.println("removing \"hi\"");
         slIter2.remove();
         System.out.println(sl);
